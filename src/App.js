4da44ae2a06ea012b6746980a64919ca274c1e3a
Sidor4ik <isidorenya.lenovo@gmail.com>
2023-01-18 17:44:03 +0300

    feat: create Contact page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Category from './pages/Category';
+import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 
 function App() {
@@ -28,7 +30,8 @@ function App() {
 	<Route path='/sign-up' element={<SignUp/>}/>
 	<Route path='/forgot-password' element={<ForgotPassword/>}/>
 	<Route path='/create-listing' element={<CreateListing/>}/>
-
+	<Route path='/category/:categoryName/:listingId' element={<Listing/>}/>
+	<Route path='/contact/:landlordId' element={<Contact/>}/>
 </Routes>
 <Navbar/>
 	 </Router>
